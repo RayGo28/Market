0cae8e66dcb446e660b2b8a23be844352f8cc364
RayGo <raytalone@gmail.com>
2026-08-24 13:34:43 +0300

    fix timestamp

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django.utils import timezone
 from .models import Coin, CoinCurrentData, PriceHistory
 
-@admin.display(description="Timestamp")
+@admin.display(description="Timestamp", ordering="timestamp")
 def timestamp_24h(obj):
     if obj.timestamp is None:
         return "No Timestamp"
-    return obj.timestamp.strftime("%d.%m.%Y %H:%M")
+    local_time = timezone.localtime(obj.timestamp)
+    return local_time.strftime("%d.%m.%Y %H:%M")
+
 
 @admin.register(Coin)
 class CoinAdmin(admin.ModelAdmin):
@@ -16,6 +19,7 @@ class CoinAdmin(admin.ModelAdmin):
 class PriceHistoryAdmin(admin.ModelAdmin):
     list_display = ("coin", "price", "market_cap", "total_volume", "price_change_percentage_24h", timestamp_24h)
     list_filter = ("coin", "timestamp")
+
 
 @admin.register(CoinCurrentData)
 class CoinCurrentDataAdmin(admin.ModelAdmin):
--- backend/core/views.py
+++ backend/core/views.py
@@ -3,11 +3,8 @@ from .models import Coin, PriceHistory
 from django.db.models import Avg, Max, Min, Count, Prefetch
 
 def index(request):
-    latest_history = PriceHistory.objects.order_by('coin', '-timestamp').distinct('coin')
 
-    coins = Coin.objects.filter(is_active=True).prefetch_related(
-        Prefetch('market_data', queryset=latest_history)
-    )
+    coins = Coin.objects.all() 
 
     return render(request, 'main/index.html', {"coins": coins})
 
